052942dcec3f356d2ad1c32f81b973749520c0ce
Andy Gibson <argibson86@gmail.com>
2015-02-11 05:03:46 +0000

    Added support code template lists

---
+++ src/MirthConnectFX/Model/CodeTemplateList.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace MirthConnectFX.Model
+{
+    [Serializable, XmlRoot("list")]
+    public class CodeTemplateList
+    {
+        [XmlElement("codeTemplate")]
+        public List<CodeTemplate> CodeTemplates { get; set; }
+    }
+}
